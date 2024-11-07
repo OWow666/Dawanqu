ac40b02714c47a021c4df21ad81b82c42613f419
Ch77ic <49362821@qq.com>
2024-11-07 16:06:30 +0800

    Update multi_linear.py

--- multi_linear.py
+++ multi_linear.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
